1fe02d4d535a87e657e877d30917be1afa786de5
agustinn <agustinveliz@gmail.com>
2023-05-16 16:37:02 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,83 @@
+     //Ejercicio 1
+     //alert ("un mensaje")
+
+     //Ejercicio 2
+     //document.write("Hello World!");
+
+     //Ejercicio 3       
+     //document.write("la suma de 5+3 es igual a " + (5+3))
+
+    //Ejercicio 4
+
+    
+    //Ejercicio 5
+
+
+    //Ejercicio 6
+
+
+    //Ejercicio 7
+
+
+    //Ejercicio 8
+
+
+    //Ejercicio 9
+    let frase = parseInt (prompt("ingrese una frase"))
+    let i=0;
+    let largodefrase = frase.lenght;
+    let cantidaddeVocale=0;
+
+    while (i < largodefrase){
+        if frase.subtring(i, i + 1) =="a" || frase.subtring
+    }
+
+        
+    
+
+
+
+
+     
+
+
+                /*Soy un primer comentario, creando mi primer conexion a JavaScript
+            alert("Hola Comision 57i")*/
+            // console.log ("hola comision 57i")
+           /* let numero1 = 4;
+            let numero2 = 6;
+            let suma;
+
+            suma = numero1 + numero2;
+            console.log(suma)
+
+            let multiplicacion;
+             multiplicacion = numero1 * numero2;
+             alert (multiplicacion)*/
+
+            //console.log(`la suma da ${5+3}`);
+             //let nombre = prompt("Nombre?");
+
+             //console.log(nombre);
+
+             /*cons secreto = math.floor(math.random()* 10)+ 1;
+
+             let numero = 0;
+
+             while(numero !== secreto){
+
+                numero = parseInt(prompt("Adivina mi numero de 1-10:"));
+
+                if ( numero === NULL){
+                    break;
+                }
+
+                if ( numero =  secreto){
+
+                    console.log ("Adivinaste!")
+                }
+                else{
+
+                    console.log("pifiaste!")
+                }
+             }*/
