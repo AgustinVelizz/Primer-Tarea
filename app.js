88349bd9e81fd814675942b83484e5dc33344af9
agustinn <agustinveliz@gmail.com>
2023-05-16 23:21:05 -0300

    Segundo commit

--- app.js
+++ app.js
@@ -1,36 +1,92 @@
      //Ejercicio 1
+     //Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
+
      //alert ("un mensaje")
 
+
      //Ejercicio 2
+     //Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
+
      //document.write("Hello World!");
 
-     //Ejercicio 3       
+
+     //Ejercicio 3 
+     //Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+
      //document.write("la suma de 5+3 es igual a " + (5+3))
 
-    //Ejercicio 4
 
-    
+    //Ejercicio 4
+    //Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+
+    //let nombredeusuario = prompt ("Coloque su nombre de usuario") 
+    //document.write(`Hola ${nombredeusuario}`);
+
+   
     //Ejercicio 5
+    //Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+
+    //let numero1 = Number.parseInt (prompt("Ingrese un numero"));
+    //let numero2 = Number.parseInt (prompt("Ingrese otro numero"));
+    //document.write(`El resultado es ${numero1+numero2}`);
 
 
     //Ejercicio 6
+    //Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+
+    //let numero1 = Number.parseInt (prompt("Ingrese un numero"));
+    //let numero2 = Number.parseInt (prompt("Ingrese otro numero"));
+    //if (numero1>numero2) {
+      //  document.write(`El primer numero ${numero1} es el mayor `);
+    //} else {
+      //  document.write(`El segundo numero ${numero2} es el mayor `);
+    //}
 
 
     //Ejercicio 7
+    //Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
 
+    //let numero1 = Number.parseInt (prompt("Ingrese primer numero"));
+    //let numero2 = Number.parseInt (prompt("Ingrese segundo numero"));
+    //let numero3 = Number.parseInt (prompt("Ingrese tercer nuemero"));
+    //if (numero1>numero2) {
+      //  if (numero1>numero3) {
+        //    document.write(`El numero ${numero1} es el mayor `);
+        //} else { 
+          //  document.write(`El numero ${numero3} es el mayor `);
+        //}
+    //} else { 
+      //  if (numero2>numero3) {
+        //    document.write(`El numero ${numero2} es el mayor `);
+        //} else {
+          //  document.write(`El numero ${numero3} es el mayor `);
+        //}
+    //}
 
     //Ejercicio 8
+    //Escribe un programa que pida un número y diga si es divisible por 2
+
+    // let numero = parseInt (prompt("Ingrese un numero"));
+
+    // if (numero %2 === 0 ) {
+    //   document.write(`El numero ${numero} es divisible por 2 `);
+
+    // } else {
+    //   document.write(`El numero ${numero} no es divisible por 2 `);
+    // }
+  
+  
+  
+  //  si son 3 iguales tienen q ser igual la cantidad y el tipo de variable
+  //  si son 2 iguales solo tienen q ser igual la cantidad
 
 
     //Ejercicio 9
-    let frase = parseInt (prompt("ingrese una frase"))
-    let i=0;
-    let largodefrase = frase.lenght;
-    let cantidaddeVocale=0;
+   // Escribe un programa que pida una frase y escriba las vocales que aparecen
+   //Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
 
-    while (i < largodefrase){
-        if frase.subtring(i, i + 1) =="a" || frase.subtring
-    }
+  let frase = prompt
+    
 
         
     
